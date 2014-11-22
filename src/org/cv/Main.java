4ff4a5f8ca3129df049ac36907f96a68197d6736
jsonRen <zhanghanbin1990@gmail.com>
2014-11-23 02:23:26 +0800

    v

--- src/org/cv/Main.java
+++ src/org/cv/Main.java
@@ -15,7 +15,6 @@ import org.cv.spider.Spider_Fetch_Main;
 import org.cv.spider.thread.SpiderStart;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-ddff
 public class Main {
 	List<Runnable> runList = new ArrayList<Runnable>();
 	Factory factory = new ConcreteFactory();
